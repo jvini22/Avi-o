1aab89885fa7e17c09fe71cf1d9f292beca46f53
João Vinicius <77241785+jvini22@users.noreply.github.com>
2023-05-11 15:35:20 -0300

    Aviao.java

---
+++ Aviao.java
@@ -0,0 +1,25 @@
+public class Aviao {
+    private String nome;
+    private String numero;
+
+    public Aviao(String nome, String numero){
+        this.nome = nome;
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+}
